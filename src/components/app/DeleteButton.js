d60528556a5a1cc5994c8043ad8c18e3dca74b1a
Karen Garcia <karen.garcia@nexscreen.com>
2019-09-23 15:50:46 -0600

    working again on Redux to figure it out

---
+++ src/components/app/DeleteButton.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import {connect} from `react-redux`;
+
+// const mapStateToProps = state => {
+//   return {colors: state.colors};
+// };
+
+// const DeleteButton = ({colors}) => (
+//         <button
+//           type="submit" 
+//           className="form-control"
+//           onClick={}
+//         >
+//           Delete
+//         </button>
+  
+
+
+// );
+
+// const DeleteButton = ({colors}) => (
+//   <ul className='list-group list-group-flush'>
+//     {colors.map(el=>(
+//       <li className='list-group-item' key={el.id}>
+//         {el.title}
+//         <button
+//           type="submit" 
+//           className="form-control"
+//           onClick={}
+//         >
+//           Delete
+//         </button>
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// const List = connect(mapStateToProps)(ConnectedList);
+
+// export default List;
